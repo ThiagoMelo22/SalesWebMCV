749bf32b588ee2cb9f42184a5278783739700072
ThiagoMelo22 <thiagomelo329@hotmail.com>
2025-11-13 16:09:55 -0300

    Simple Create form

--- SalesWebMcv/Controllers/SellersController.cs
+++ SalesWebMcv/Controllers/SellersController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SalesWebMcv.Models;
 using SalesWebMcv.Services;
 
 namespace SalesWebMcv.Controllers
@@ -6,16 +8,40 @@ namespace SalesWebMcv.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _department;
 
-        public SellersController(SellerService sellerService) 
+        public SellersController(SellerService sellerService, DepartmentService department) 
         {
             _sellerService = sellerService;
+            _department = department;
         }
 
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() 
+        {
+            var departments = _department.FindAll();
+            ViewData["DepartmentId"] = new SelectList(departments, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _sellerService.Insert(seller);
+                return RedirectToAction(nameof(Index));
+            }
+
+            var departments = _department.FindAll();
+            ViewData["DepartmentId"] = new SelectList(departments, "Id", "Name", seller.DepartmentId);
+            return View(seller);
         }
     }
 }
---
+++ SalesWebMcv/Services/DepartmentService.cs
@@ -0,0 +1,19 @@
+﻿using SalesWebMcv.Models;
+
+namespace SalesWebMcv.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMcvContext _context;
+
+        public DepartmentService(SalesWebMcvContext context) 
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.ToList();
+        }
+    }
+}
